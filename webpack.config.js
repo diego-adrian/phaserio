32d945ee8b07c65af6b4dd5b4c55e96e2947cea4
Adrian <dbarra@agetic.gob.bo>
2020-12-22 15:46:38 -0400

    Webpack 5

--- webpack.config.js
+++ webpack.config.js
@@ -1,58 +1,58 @@
-const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const { CleanWebpackPlugin } = require("clean-webpack-plugin");
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const webpack = require("webpack");
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin'); // Para injectar el bundle a un template
+const { CleanWebpackPlugin } = require('clean-webpack-plugin'); // Para limpiar todo lo que se quede en dist
+const MiniCssExtractPlugin = require('mini-css-extract-plugin'); // Para separar todo lo css del bundle
+const webpack = require('webpack');
 
 module.exports = {
   entry: {
-    main: path.resolve(__dirname, "./src/index"),
+    main: path.resolve(__dirname, './src/index')
   },
   output: {
-    path: path.resolve(__dirname, "./dist"),
-    filename: "[name].bundle.js",
+    path: path.resolve(__dirname, './dist'),
+    filename: '[name].[hash].bundle.js'
   },
   plugins: [
     new HtmlWebpackPlugin({
-      title: "Phaser",
-      template: path.resolve(__dirname, "./src/template.html"),
-      filename: "index.html",
+      title: 'Phaser',
+      template: path.resolve(__dirname, './src/template.html'),
+      filename: 'index.html'
     }),
     new CleanWebpackPlugin(),
     new MiniCssExtractPlugin(),
-    new webpack.HotModuleReplacementPlugin(),
+    new webpack.HotModuleReplacementPlugin()
   ],
   module: {
     rules: [
       {
         test: /\.(js)$/,
         exclude: /node_modules/,
-        use: ["babel-loader"],
+        use: ['babel-loader']
       },
       // Para imagenes
       {
         test: /\.(?:ico|gif|png|jpg|jpeg)$/i,
-        type: "asset/resource",
+        type: 'asset/resource'
       },
       // Para fuentes o archivos svg
       {
         test: /\.(wolff(2)?|eot|ttf|otf|svg)$/,
-        type: "asset/inline",
+        type: 'asset/inline'
       },
       // Para estilos scss / css
       {
         test: /\.(scss|css)$/,
-        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
-      },
-    ],
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader']
+      }
+    ]
   },
-  mode: "development",
+  mode: 'development',
   devServer: {
     historyApiFallback: true,
-    contentBase: path.resolve(__dirname, "./dist"),
+    contentBase: path.resolve(__dirname, './dist'),
     open: true,
     compress: true,
     hot: true,
-    port: 9090,
-  },
+    port: 9090
+  }
 };
